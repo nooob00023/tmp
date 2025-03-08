766f6a622c887ceec9a204f6edd0aebfcf4e409f
nooob00023 <nana1000000002@gmail.com>
2025-03-08 15:16:49 +0700

    Upload file 1741421809499.php

---
+++ uploads/1741421809499.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Hello, World!</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            text-align: center;
+        }
+        h1 {
+            color: blue;
+        }
+    </style>
+</head>
+<body>
+    <h1><?php echo "Hello, world!"; ?></h1>
+</body>
+</html>
